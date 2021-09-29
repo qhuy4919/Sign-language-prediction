e8bdaf19195b62562deb20de40cb4d177190fe7e
Qhuy <65387927+qhuy4919@users.noreply.github.com>
2021-09-29 21:27:29 +0700

    create model layout

---
+++ gesturePrediction/model.py
@@ -0,0 +1,11 @@
+import skimage
+import pandas as pd
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import warnings
+warnings.filterwarnings('ignore')
+# params
+print(skimage.__version__)
