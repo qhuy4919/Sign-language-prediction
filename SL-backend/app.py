8fbddae18d028ddf9058d96f6606ea0c79264416
Qhuy <65387927+qhuy4919@users.noreply.github.com>
2021-11-21 21:42:14 +0700

    stream video with mediapipe

--- SL-backend/app.py
+++ SL-backend/app.py
@@ -7,13 +7,15 @@ import re
 import numpy as np
 import cv2
 
+#hand tracking modul
+import handTracking as htm 
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, request, render_template, Response
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
@@ -25,10 +27,16 @@ MODEL_PATH = 'model/SL_model5.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
+
+#load video captured 
+camera=cv2.VideoCapture(0)
+
+#load hand tracking module 
+detector = htm.handDetector(detectionCon=0.75)
 
 print('Model loaded. Check http://127.0.0.1:5000/')
+contours_size = 100
+padding = 55
 
 def createLabel(label):
     for i in range(ord('A'), ord('Z')+1):
@@ -58,22 +66,83 @@ def preprocessing(img):
     img = equalize(img)
     return img
 
-def model_predict(img_path, model):
-    x = cv2.imread(img_path)
+def model_predict(img):
+    x = cv2.imread(img)
     # Preprocessing the image
     x = np.array(x)   
     x = cv2.resize(x,(64,64))
     x = preprocessing(x)
     x = x.reshape(1, 64, 64, 1)
-    # x = np.true_divide(x, 255)
-    # x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    # x = preprocess_input(x, mode='caffe')
-
     preds = model.predict(x)
     return preds
+
+def reshape_contours(x0, x1, y0, y1, padding):
+    if(abs(x0 - x1) > abs(y0 - y1)):
+        padding_0 = padding
+        x0_new = x0 - padding_0
+        x1_new = x1 + padding_0
+        len_axis_x = abs(x0 - x1) + 2*padding_0
+        padding_1 = (len_axis_x - abs(y0 - y1))//2
+        y0_new, y1_new = y0 - padding_1, y1 + padding_1
+
+        return x0_new, x1_new, y0_new, y1_new
+    else:
+        padding_0 = padding
+        y0_new = y0 - padding_0
+        y1_new = y1 + padding_0
+        len_axis_y = abs(y0 - y1) + 2*padding_0
+        padding_1 = (len_axis_y - abs(y0 - y1))//2
+        x0_new, x1_new = x0 - padding_1, x1 + padding_1
+
+        return x0_new, x1_new, y0_new, y1_new
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        
+        success,frame=camera.read()      
+        success01, landmarks = detector.findHands(frame)
+        if not success:
+            break
+        else:
+              #
+            try:
+
+                min_x = 0
+                min_y = 0
+                max_x = 0
+                max_y = 0
+                if len(landmarks) > 0:
+                    xx, yy = zip(*landmarks)
+                    min_x = min(xx)
+                    min_y = min(yy)
+                    max_x = max(xx)
+                    max_y = max(yy)
+                    # cv2.rectangle(image, (min_x, min_y),
+                    #             (max_x, max_y), (0, 255, 255), 2)
+                    if max_x//min_x == max_y//min_y:
+                        cv2.rectangle(frame, (min_x-padding, min_y - padding),
+                                    (max_x + padding, max_y + padding), (0, 255, 0), 2)
+                        min_x, max_x, min_y, max_y =  min_x-padding, max_x + padding, min_y - padding, max_y + padding
+
+                    else:
+                        min_x, max_x, min_y, max_y = reshape_contours(
+                            min_x, max_x, min_y, max_y, padding)
+                        cv2.rectangle(frame, (min_x, min_y),
+                                    (max_x, max_y), (0, 255, 0), 2)
+            except Exception as e:
+                print(e)
+
+            ret,buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
+
 
 
 @app.route('/', methods=['GET'])
@@ -95,16 +164,15 @@ def upload():
 
         # Make prediction
         preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
         label = createLabel({})
-        print('000000000000000000000000000----------------------------',preds)
         result = label[np.argmax(preds)]
-        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        # result = str(pred_class[0][0][1])               # Convert to string
+
         return result
     return None
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
